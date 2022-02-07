4e2e9d0a9239bcc83039c795b05ae266c6b7cb97
yalong <4379711@qq.com>
2022-02-08 02:18:10 +0800

    打通lol客户端API接口

--- src/main/java/yalong/site/LeagueClientHelper.java
+++ src/main/java/yalong/site/LeagueClientHelper.java
@@ -1,10 +1,21 @@
 package yalong.site;
 
+import yalong.site.bo.LeagueClientBO;
+import yalong.site.services.LinkLeagueClientService;
+import yalong.site.utils.ProcessUtil;
+import yalong.site.utils.RequestUtil;
+
+import java.io.IOException;
+
 /**
  * @author yaLong
  */
 public class LeagueClientHelper {
-    public static void main(String[] args) {
-        System.out.print("hello");
+    public static void main(String[] args) throws IOException {
+        LeagueClientBO leagueClientBO = ProcessUtil.getClientProcess();
+        LinkLeagueClientService leagueClientService = new LinkLeagueClientService();
+        RequestUtil requestUtil = new RequestUtil(leagueClientBO);
+        leagueClientService.setRequestUtil(requestUtil);
+        String loginInfo = leagueClientService.getLoginInfo();
     }
 }
---
+++ src/main/java/yalong/site/bo/LeagueClientBO.java
@@ -0,0 +1,13 @@
+package yalong.site.bo;
+
+import lombok.Data;
+
+/**
+ * @author yaLong
+ */
+@Data
+public class LeagueClientBO {
+    private String port;
+
+    private String token;
+}
