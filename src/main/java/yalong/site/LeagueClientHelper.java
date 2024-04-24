b3858bc43806ab33fc3043a6c38d956aeaad841e
yalong <4379711@qq.com>
2024-04-24 17:22:16 +0800

    启动!

--- src/main/java/yalong/site/LeagueClientHelper.java
+++ src/main/java/yalong/site/LeagueClientHelper.java
@@ -9,19 +9,19 @@ public class LeagueClientHelper {
 
 	public static void main(String[] args) {
 		MainFrame.start();
-//		while (true) {
-//			try {
-//				ClientStarter.start();
-//			} catch (Exception e) {
-//				MainFrame.hiddenFrame();
-//				int running = MainFrame.continueRun(e.getMessage());
-//				if (running == 1) {
-//					System.exit(0);
-//				} else {
-//					MainFrame.showFrame();
-//				}
-//			}
-//		}
+		while (true) {
+			try {
+				ClientStarter.start();
+			} catch (Exception e) {
+				MainFrame.hiddenFrame();
+				int running = MainFrame.continueRun(e.getMessage());
+				if (running == 1) {
+					System.exit(0);
+				} else {
+					MainFrame.showFrame();
+				}
+			}
+		}
 	}
 
 }
